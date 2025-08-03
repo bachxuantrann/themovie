02dc255fe41e0201f612cc4305b6d7cfc4504c74
bachxuantrann <bxt203@gmail.com>
2025-08-03 14:06:20 +0700

    init prj

---
+++ src/test/java/com/ptit_intern/themoviedb/ThemoviedbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ptit_intern.themoviedb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThemoviedbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
